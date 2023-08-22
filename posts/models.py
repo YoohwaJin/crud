d524f9a1a0af46236ae1d904950ccebb28ddb9f8
YoohwaJin <qudghkehdtod@naver.com>
2023-08-22 10:28:54 +0900

    모델 정의

--- posts/models.py
+++ posts/models.py
@@ -1,3 +1,8 @@
 from django.db import models
 
 # Create your models here.
+class Post(models.Model):
+    title = models.CharField(max_length=100) #CharField에는 항상 max_length를 설정해야함
+    content = models.TextField()
+
+# 여기까지가 모델을 정의한 것
